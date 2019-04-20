2a5388ba01e2b7e32e6b97946f350952db02befc
Miguel Serra <Miguel.Serra@criticaltechworks.com>
2019-04-20 13:19:19 +0100

    NA: Chapter 5

--- harp/components/app/app.js
+++ harp/components/app/app.js
@@ -3,6 +3,13 @@ import Strings from '../strings/strings.js';
 export default class WebHarpApp extends HTMLElement {
     connectedCallback() {
         this.innerHTML = '<webharp-strings strings="' + this.getAttribute('strings') + '"></webharp-strings>';
+        this.stringsElement = this.querySelector('webharp-strings');
+        this.addEventListener('mousemove', e => this.onMouseMove(e));
+    }
+
+    onMouseMove(event) {
+        this.stringsElement.points = { last: this.lastPoint, current: { x: event.pageX, y: event.pageY } };
+        this.lastPoint = { x: event.pageX, y: event.pageY };
     }
 }
 
--- harp/components/string/string.js
+++ harp/components/string/string.js
@@ -1,19 +1,61 @@
+import MIDI from '../../midijs.wrapper.js'
+
 export default class String extends HTMLElement {
     strum(params) {
+        if (this.timer) { clearTimeout(this.timer); }
+
+        let dur = params.power * 10 + 250;
+        this.classList.add('shake', 'shake-constant', 'shake-horizontal');
+        if (dur < 500) {
+            this.classList.add('shake-little');
+        }
+        this.timer = setTimeout( () => this.stopStrum(), dur);
+        this.playSound(params);
+    }
+
+    playSound(params) {
+        if (!this._ready) { return; }
+        let note = 45 + params.string * 5;
+        MIDI.setVolume(0, 127);
+        MIDI.noteOn(0, note, params.power, 0);
+        MIDI.noteOff(0, note, 0.75);
+    }
+
+    touchStarted() {
+        if(typeof AudioContext != "undefined" || typeof webkitAudioContext != "undefined") {
+            let resumeAudio = function() {
+                if(typeof g_WebAudioContext == "undefined" || g_WebAudioContext == null) return;
+                if(g_WebAudioContext.state == "suspended") g_WebAudioContext.resume();
+                document.removeEventListener("click", resumeAudio);
+            };
+            document.addEventListener("click", resumeAudio);
+        }
     }
 
     stopStrum() {
+        this.classList.remove('shake', 'shake-constant', 'shake-horizontal', 'shake-little');
     }
 
     connectedCallback() {
-        this.innerHTML = `<div class="line"></div>
-                          <style>
-                              webharp-string > .line { 
-                                  background-color: white;
-                                  height: 100%;
-                                  width: 2px; 
-                              }
-                          </style>`;
+        this.touchStarted();
+        MIDI.loadPlugin({
+            soundfontUrl: './',
+            instrument: 'acoustic_grand_piano',
+            onsuccess: () => this.onLoaded()
+        });
+
+        this.innerHTML = '<div class="line"></div> \
+                      <style>\
+                          webharp-string > .line { \
+                              background-color: white;\
+                              height: 100%; \
+                              width: 2px; \
+                          }\
+                      </style>';
+    }
+
+    onLoaded() {
+        this._ready = true;
     }
 }
 
